4b9a5f23b5e498be17017fd09aaf902a0d1acc6f
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-11-10 10:31:28 +0100

    Add a function to extract the StackTrace
    This also remove the type parameter of StackFrame

--- Control/Monad/Failure.hs
+++ Control/Monad/Failure.hs
@@ -7,10 +7,12 @@ module Control.Monad.Failure
   , StackFrame(..)
   , stackFrame
   , failureAt
+  , stackTrace
   ) where
 
 import Control.Monad.Failure.Class
 import Data.Typeable
+import Data.Maybe (fromMaybe)
 import qualified Control.Exception as E
 
 -- ! 'StackFrame's decorate exceptions with some location data.
@@ -19,22 +21,31 @@ import qualified Control.Exception as E
 -- However the 'StackTrace' is kept and augemented while used
 -- in pure code. Using for instance 'MonadLoc', 'withLoc' and
 -- 'withLocTH'.
-data StackFrame e = StackFrame { stackFrameText      :: String
-                               , stackFrameException :: e
-                               }
-  deriving (Eq, Ord, Typeable)
+data StackFrame = StackFrame { stackFrameText      :: String
+                             , stackFrameException :: E.SomeException
+                             }
+  deriving (Typeable)
 
 -- ! Builds a stack frame given a source location text and
 -- an inner exception.
-stackFrame :: E.Exception e => String -> e -> StackFrame e
-stackFrame = StackFrame
+stackFrame :: E.Exception e => String -> e -> StackFrame
+stackFrame loc = StackFrame loc . E.SomeException
 
-instance E.Exception e => E.Exception (StackFrame e) where
-  toException   = E.toException . stackFrameException
-  fromException
-    = ((fmap (stackFrame "") . E.fromException) =<<) . cast
+-- | Extracts a stack trace from any exception. This basically
+-- look for a chain of 'StackFrame's, and stops on any other
+-- exception.
+stackTrace :: E.Exception e => e -> [String]
+stackTrace e =
+  case cast e :: Maybe StackFrame of
+    Nothing -> []
+    Just (StackFrame loc (E.SomeException e')) -> loc : stackTrace e'
 
-instance Show e => Show (StackFrame e) where
+instance E.Exception StackFrame where
+  toException sf = case stackFrameException sf of
+                     E.SomeException e -> E.toException e
+  fromException e = Just . fromMaybe (StackFrame "" e) . cast $ e
+
+instance Show StackFrame where
   showsPrec p (StackFrame "" e)
     = showsPrec p e
   showsPrec p (StackFrame txt e)
@@ -42,5 +53,5 @@ instance Show e => Show (StackFrame e) where
 
 -- | Like 'failure', but wraps the exception with a 'StackFrame'
 -- of the given source location text.
-failureAt :: MonadFailure (StackFrame e) m => String -> e -> m a
-failureAt txt e = failure (StackFrame txt e)
+failureAt :: (E.Exception e, MonadFailure StackFrame m) => String -> e -> m a
+failureAt txt e = failure (StackFrame txt (E.SomeException e))
